7d0c2f756bd32e4e8cea551f47a3c52a8be8c7e5
andrea <florezandre904@gmail.com>
2026-02-20 16:14:45 -0500

    en proceso

---
+++ src/taller4/Taller4.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author UIS
+ */
+public class Taller4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static void ejercicio1 () {
+        while (true) {
+            int menu = Integer.parseInt(JOptionPane.showInputDialog("menu:\n"
+                + "1 Registrar estudiante.\n"
+                + "2 Consultar estudiante.\n"
+                + "3 Modificar nombre del estudiante.\n"
+                + "4 Modificar Telefono del estudiante.\n"
+                + "5 Eliminar estudiante.\n"
+                + "6 Imprimir listado.\n "
+                + "7 salir\n"));            
+            switch (menu) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    System.exit(0);
+                    break;
+                
+            }
+        }
+    }   
+}
